bdb58f880165dafbd2492a8e8cbf2fa7926acd2f
To Minh Anh <minhhanhs.2205@gmail.com>
2024-12-05 19:56:43 +0700

    Revert "make method"
    This reverts commit 0fb7d9eeaf70e177f9c09e9ab56ca063e382dbd8.

--- sourcecode/src/model/Square.java
+++ sourcecode/src/model/Square.java
@@ -1,44 +1,5 @@
 package model;
 
-import model.Cell;
-import java.util.List;
-
-import gems.Gems;
-
-import java.util.ArrayList;
-
 public class Square {
 
-    private int position;
-   private List<Gems> gems;
-
-    // public Square(int position) {
-    //     this.position = position;
-    //     this.gems = new ArrayList<>();
-    // }
-
-    // public void addGem(Gems gem) {  
-        
-    // }
-
-    public void removeGem() {
-    
-    }
-
-    public int getGemsCount() {
-            return position;
-   
-    }
-
-    public boolean isEmpty() {
-            return false;
-        
-    }
-
-    public boolean isPickable() {
-
-        return true;
-    }
 }
-
-    
