e313e8e6b4c1200ba92ad983de9b957440dcfabc
bachhungcb <bachhungcb@gmail.com>
2024-12-05 14:52:07 +0700

    add method for GameBoard class
    these methods include getter,setter, constructor, showMenu

--- sourcecode/src/model/GameBoard.java
+++ sourcecode/src/model/GameBoard.java
@@ -1,5 +1,40 @@
 package model;
 
-public class GameBoard {
+import java.util.List;
 
+public class GameBoard {
+    private List<Cell> cells;
+
+    // Constructor
+    public GameBoard(List<Cell> cells) {
+        this.cells = cells;
+    }
+
+    // Getter
+    public List<Cell> getCells() {
+        return cells;
+    }
+
+    // Setter
+    public void setCells(List<Cell> cells) {
+        this.cells = cells;
+    }
+
+    // Get the score of a player
+    public int getScore(Player player) {
+        return player.getScore();
+    }
+
+    // Get the name of a player
+    public String getPlayerName(Player player) {
+        return player.getName();
+    }
+
+    // Display the game menu
+    public void showMenu() {
+        // TODO: Implement the menu display logic
+        System.out.println("1. Start Game");
+        System.out.println("2. View Scores");
+        System.out.println("3. Exit");
+    }
 }
