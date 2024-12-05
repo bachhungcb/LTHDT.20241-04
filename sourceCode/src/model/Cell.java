f5b9e668e084ec981ff023124181ed6bb6bc9ba1
Monsiertuna <macthanhbinhtn@gmail.com>
2024-12-05 20:30:52 +0700

    create haflCircle

--- sourceCode/src/model/Cell.java
+++ sourceCode/src/model/Cell.java
@@ -8,4 +8,24 @@ public abstract class Cell {
     private List<Gems> gems;
     private int position;
     private boolean isUpperRow;
+	public List<Gems> getGems() {
+		return gems;
+	}
+	public void setGems(List<Gems> gems) {
+		this.gems = gems;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public boolean isUpperRow() {
+		return isUpperRow;
+	}
+	public void setUpperRow(boolean isUpperRow) {
+		this.isUpperRow = isUpperRow;
+	}
+    
+    
 }
---
+++ sourceCode/src/model/halfCircle.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+import gems.Gems;
+
+public class halfCircle extends Cell {
+	private int position;
+	private List<Gems> gems;
+
+	public void addGem(Gems gems) {
+
+	}
+
+	public void removeGem() {
+
+	}
+
+	public int getGemsCount() {
+		return 0;
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+}
