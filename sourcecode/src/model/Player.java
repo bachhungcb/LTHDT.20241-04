4c969c8ef1a5a70f73e95da656d4813e858bdf61
bachhungcb <bachhungcb@gmail.com>
2024-12-05 10:43:53 +0700

    add attributes for Player class

---
+++ sourcecode/src/model/Player.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Player {
+    private int playerID;
+    private String name;
+    private int score;
+    private boolean isUpperRow;
+    private Cell handPosition;
+    private boolean inTurn;
+    private int numOfGemsInHand;
+    private int numOfGemsLent;
+    private int numOfGemsBorrowed;
+
+
+
+}
