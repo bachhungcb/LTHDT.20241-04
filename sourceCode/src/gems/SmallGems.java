ae264d5188943194280c91639f202a0220dc9b3a
HoangAnh226076 <163961667+HoangAnh226076@users.noreply.github.com>
2024-12-05 23:35:29 +0700

    tao cac method cho class SmallGems

---
+++ sourceCode/src/gems/SmallGems.java
@@ -0,0 +1,26 @@
+package gems;
+
+import model.Cell;
+
+public class SmallGems extends Gems {
+	private int value = 5;
+	public void smallGems(Cell position) {
+		
+	}
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public Cell getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
