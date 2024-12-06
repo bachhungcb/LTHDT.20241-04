0605dd699fe4d25d16c3b2f0e7a2384f7764fd41
tranvananhanhanh <139228186+tranvananhanhanh@users.noreply.github.com>
2024-12-06 17:54:42 +0700

    Update Cell.java

--- sourceCode/src/model/Cell.java
+++ sourceCode/src/model/Cell.java
@@ -1,11 +1,11 @@
 package model;
 
-import gems.Gems;
+import gem.Gem;
 
 import java.util.List;
 
 public abstract class Cell {
-    private List<Gems> gems;
+    private List<Gem> gem;
     private int position;
     private boolean isUpperRow;
 }
