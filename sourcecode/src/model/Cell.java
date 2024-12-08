e6a695635506ee0d4c783d8f15bcf83d71b1b6e7
bachhungcb <bachhungcb@gmail.com>
2024-12-08 22:08:29 +0700

    add getter for Cell class

--- sourcecode/src/model/Cell.java
+++ sourcecode/src/model/Cell.java
@@ -5,4 +5,18 @@ public abstract class Cell {
     private List<Gem> gems;
     private int position;
     private boolean isUpperRow;
+
+    public List<Gem> getGems() {
+        return gems;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public boolean isUpperRow() {
+        return isUpperRow;
+    }
+
+
 }
