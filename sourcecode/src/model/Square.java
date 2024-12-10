031dd7f9810db8c585765f8053ac31ee7964861e
Monsiertuna <macthanhbinhtn@gmail.com>
2024-12-11 01:25:00 +0700

    test addGem

--- sourcecode/src/model/Square.java
+++ sourcecode/src/model/Square.java
@@ -14,37 +14,34 @@ public class Square {
         this.gems = new ArrayList<>();
     }
 
-	//Phương thức thêm sỏi vào bán cầu
-	public void addGem(Gem gem) {
-		gems.add(gem);
-	}
+    // Phương thức thêm sỏi vào bán cầu
+    public void addGem(Gem gem) {
+        gems.add(gem);
+    }
 
     public void removeGem() {
-      
+
     }
 
     public int getGemsCount() {
-            return position;
-   
+        return position;
     }
 
     public boolean isEmpty() {
-            return false;
-        
+        return false;
     }
 
     public boolean isPickable() {
-
         return true;
     }
-    
-    
 
+    public static void main(String[] args) {
+    	Square square = new Square(1);
+        Gem gem1 = new Gem();
+        Gem gem2 = new Gem();
+        square.addGem(gem1);
+        square.addGem(gem2);
+        System.out.println("Add 2 gems to square 1");
+    }
 
-
-  
-
-    
 }
-
-    
