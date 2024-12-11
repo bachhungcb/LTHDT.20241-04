a1dd546d82f34de80d0a6064d26d33b394abd368
To Minh Anh <minhhanhs.2205@gmail.com>
2024-12-11 18:24:08 +0700

    Update Game.java

--- sourcecode/src/game/Game.java
+++ sourcecode/src/game/Game.java
@@ -35,7 +35,7 @@ public class Game {
 
     private boolean checkGameOver(){
         //TODO:  kiểm tra lại cách List<Cell> cells ở gameboard có trả về HalfCircle không.
-        if(Cell.isGameOver()){
+        if(Cell.isGameOver(boảd.getCells)){
             Player winner = determineWinner();
             System.out.println("Game over! Winner: " + winner.toString());
             return true;
