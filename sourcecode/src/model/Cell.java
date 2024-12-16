df9a6941cef38f320a88a3e0f505d7b8949ee473
Monsiertuna <macthanhbinhtn@gmail.com>
2024-12-16 22:49:42 +0700

    Constructor
    tạo phương thức khởi tạo của Cell

--- sourcecode/src/model/Cell.java
+++ sourcecode/src/model/Cell.java
@@ -1,37 +1,42 @@
 package model;
 
 import gems.Gems;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Cell {
-    private List<Gems> gems;
-    private int position;
-    private boolean isUpperRow;
+	private List<Gems> gems;
+	private int position;
+	private boolean isUpperRow;
+
 	public List<Gems> getGems() {
 		return gems;
 	}
+
 	public void setGems(List<Gems> gems) {
 		this.gems = gems;
 	}
+
 	public int getPosition() {
 		return position;
 	}
+
 	public void setPosition(int position) {
 		this.position = position;
 	}
+
 	public boolean isUpperRow() {
 		return isUpperRow;
 	}
+
 	public void setUpperRow(boolean isUpperRow) {
 		this.isUpperRow = isUpperRow;
 	}
-	
-    public Cell(int position, boolean isUpperRow, int numberOfGems) {
-        this.gems = new ArrayList<>(numberOfGems); 
-        this.position = position;
-        this.isUpperRow = isUpperRow; 
-    }
-    
-    
+
+	public Cell(int position, boolean isUpperRow, int numberOfGems) {
+		this.gems = new ArrayList<>(numberOfGems);
+		this.position = position;
+		this.isUpperRow = isUpperRow;
+	}
+
 }
