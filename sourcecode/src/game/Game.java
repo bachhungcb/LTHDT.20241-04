21d69c9a32fd6d552b144b93946a0a681d5280f5
bachhungcb <bachhungcb@gmail.com>
2024-12-17 17:33:19 +0700

    delete Test class and make Game class startable

--- sourcecode/src/game/Game.java
+++ sourcecode/src/game/Game.java
@@ -1,13 +1,14 @@
 package game;
 
-import gem.Gem;
 import model.Cell;
 import model.GameBoard;
 import model.Player;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
+    static Scanner scanner = new Scanner(System.in);
     private Player player1;
     private Player player2;
     private GameBoard board;
@@ -17,31 +18,86 @@ public class Game {
         this.player2 = player2;
         this.board = board;
     }
-    public void startGame() {
-        player1.setInTurn(Math.random() < 0.5);
-        System.out.println("Game has started!");
-        System.out.println((player1.isInTurn() ? player1.getName() : player2.getName()) + " plays first.");
-        playGame();
 
-    }
-    private boolean checkGameOver() {
-        return Cell.isGameOver(board.getCells());
-    }
- // Method to simulate playing the game
-    private void playGame() {
-    	//TODO
-        // Assume there's a game loop here to continue until the game is over
-        while (!checkGameOver()) {
-            System.out.println((player1.isInTurn() ? player1.getName() : player2.getName()) + "'s turn.");
-            
-            // Player's turn logic
-            //makeMove();
+    // Method to simulate playing the game
+ public static void GameStart(Player player1, Player player2, GameBoard board){
+     ArrayList<Cell> Player1CellOnSide = new ArrayList<>();
+     ArrayList<Cell> Player2CellOnSide = new ArrayList<>();
 
-            switchTurn();  // Switch turn after each move
-        }
+     Cell[] boardList = board.getBoard();
 
-        endGame();  // End the game when it's over
-    }
+     for(int i = 1; i <= 5; i++){
+         Player1CellOnSide.add(boardList[i]);
+     }
+
+     for(int i = 7; i <= 11; i++) {
+         Player2CellOnSide.add(boardList[i]);
+     }
+
+     player1.setCellsOnSide(Player1CellOnSide);
+     player2.setCellsOnSide(Player2CellOnSide);
+
+     while (!(Cell.isGameOver(boardList))){
+         System.out.println("********************************************");
+         System.out.println("Player 1's turn");
+         System.out.println("Player 1 chooses a cell to pick gems from: ");
+         int cellNum = scanner.nextInt();
+         scanner.nextLine();
+
+         // Modify to check if the cell is in player1's side
+         while (!player1.getCellsOnSide().contains(boardList[cellNum]) || !player1.pickUpGemFrom(boardList[cellNum])){
+             System.out.println("Invalid cell. Please choose a cell from your side!");
+             cellNum = scanner.nextInt();
+             scanner.nextLine();
+         }
+
+         System.out.println("Player 1 chooses Hand Direction");
+         int Player1HandDirection = scanner.nextInt();
+         scanner.nextLine();
+
+         while (Player1HandDirection != 1 && Player1HandDirection != 0) {
+             System.out.println("Invalid Hand Direction. Please choose again!");
+             Player1HandDirection = scanner.nextInt();
+         }
+         player1.spreadGem(boardList[cellNum], Player1HandDirection, board);
+
+         for(Cell cell : boardList){
+             System.out.println(cell.toString());
+         }
+
+         System.out.println("********************************************");
+         System.out.println("Player 2's turn");
+         System.out.println("Player 2 chooses a cell to pick gems from: ");
+         int cellNum2 = scanner.nextInt();
+         scanner.nextLine();
+
+         // Modify to check if the cell is in player2's side
+         while (!player2.getCellsOnSide().contains(boardList[cellNum2]) || !player2.pickUpGemFrom(boardList[cellNum2])){
+             System.out.println("Invalid cell. Please choose a cell from your side!");
+             cellNum2 = scanner.nextInt();
+             scanner.nextLine();
+         }
+
+         System.out.println("Player 2 chooses Hand Direction");
+         int Player2HandDirection = scanner.nextInt();
+         scanner.nextLine();
+
+         while (Player2HandDirection != 1 && Player2HandDirection != 0) {
+             System.out.println("Invalid Hand Direction. Please choose again!");
+             Player2HandDirection = scanner.nextInt();
+         }
+         player2.spreadGem(boardList[cellNum2], Player2HandDirection, board);
+
+         for(Cell cell : boardList){
+             System.out.println(cell.toString());
+         }
+         System.out.println(player1.getName() + ":" +player1.getScore());
+         System.out.println(player2.getName() + ":" +player2.getScore());
+     }
+
+     Player winner = Game.determineWinner(player1,player2);
+     System.out.println("Game over! Winner: " + winner);
+ }
 
     private void endGame(){
         //TODO: Kết thúc game
@@ -59,16 +115,6 @@ public class Game {
         }
     }
 
-//    private boolean checkGameOver(){
-//        //TODO:  kiểm tra lại cách List<Cell> cells ở gameboard có trả về HalfCircle không.
-//        if(Cell.isGameOver(board.getCells())){
-//            Player winner = determineWinner();
-//            System.out.println("Game over! Winner: " + winner.toString());
-//            return true;
-//        }
-//        return false;
-//    }
-
     //Check who is the winner by comparing score
     public static Player determineWinner(Player player1, Player player2){
         if(player1.getScore() > player2.getScore()){
@@ -82,23 +128,22 @@ public class Game {
         return null;
     }
 
-    private boolean validateMove(){
-        //TODO: validate Move
-        return false;
-    }
 
-    //this is function to calculate the score of a cell
-    //not to calculate the final score
-    private int calculateScore(Cell cell){
-        int score = 0;
-        List<Gem> gems = cell.getGems();
+    public static void main(String[] args){
+        GameBoard board = new GameBoard();
+        Player Player1 = new Player(1, "Bach", 0, false);
+        Player Player2 = new Player(2, "Huy", 0, true);
 
-        if (gems != null) {
-            for (Gem gem : gems) {
-                score += gem.getValue();
-            }
+        System.out.println("Introduction");
+        Player1.setInTurn((Math.random()) < 0.5);
+        if(Player1.isInTurn()){
+            System.out.println("Player 1 play first");
+            GameStart(Player1, Player2, board);
+
+        }else{
+            System.out.println("Player 2 play first");
+            GameStart(Player2, Player1, board);
         }
 
-        return score;
     }
 }
