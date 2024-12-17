d4e9b58f10eb89fd972c05fc9c75a65e32e2e8c1
bachhungcb <bachhungcb@gmail.com>
2024-12-17 16:45:14 +0700

    update method borrowGems
    update cells in borrowGems to match with list of cell

--- sourcecode/src/model/Player.java
+++ sourcecode/src/model/Player.java
@@ -125,7 +125,7 @@ public class Player {
 	    int[] positions1 = new int[]{1, 2, 3, 4, 5};
 	    int[] positions2 = new int[]{6, 7, 8, 9, 10};
 
-	    List<Cell> cells = board.getCells(); // Giả định GameBoard có danh sách các ô
+	    List<Cell> cells = List.of(board.getCells()); // Giả định GameBoard có danh sách các ô
 
 	    // Kiểm tra các ô trong hàng trên hoặc dưới
 	    if (this.isUpperRow()) {
