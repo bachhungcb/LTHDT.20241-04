a71e056fb8c0004573862d84aaede91f19b4cffb
bachhungcb <bachhungcb@gmail.com>
2024-12-24 16:17:28 +0700

    feat: add shareData to display winner

--- sourcecode/screen/application/ShareData.java
+++ sourcecode/screen/application/ShareData.java
@@ -4,31 +4,24 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class ShareData {
-    private StringProperty winnerName = new SimpleStringProperty();
-    private StringProperty winnerScore = new SimpleStringProperty();
-
-    public void setWinnerName(String winnerName) {
-        this.winnerName.set(winnerName);
-    }
-
-    public void setWinnerScore(String winnerScore) {
-        this.winnerScore.set(winnerScore);
-    }
-
-    public String getWinnerName() {
-        return winnerName.get();
-    }
+    private final StringProperty winnerName = new SimpleStringProperty();
+    private final StringProperty winnerScore = new SimpleStringProperty();
 
     public StringProperty winnerNameProperty() {
         return winnerName;
     }
 
-    public String getWinnerScore() {
-        return winnerScore.get();
-    }
-
     public StringProperty winnerScoreProperty() {
         return winnerScore;
+    }
+
+    // Setters and Getters for the properties
+    public void setWinnerName(String name) {
+        winnerName.set(name);
+    }
+
+    public void setWinnerScore(String score) {
+        winnerScore.set(score);
     }
 
 }
