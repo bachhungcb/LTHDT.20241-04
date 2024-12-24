c84195a9d2ab140c41bd8be8703c851d567518a5
bachhungcb <bachhungcb@gmail.com>
2024-12-24 16:17:13 +0700

    feat: add shareData to display winner

--- sourcecode/screen/controller/EndScreenController.java
+++ sourcecode/screen/controller/EndScreenController.java
@@ -19,31 +19,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public  class EndScreenController implements Initializable {
+public class EndScreenController implements Initializable {
     private final GameBoard board;
     private final Player player1, player2;
-    private PlayScreenController playScreenController;
-    private ShareData data;
-    public EndScreenController(GameBoard board, Player player1, Player player2) {
+    private final ShareData data;
+    public EndScreenController(GameBoard board, Player player1, Player player2, ShareData data) {
         this.board = board;
         this.player1 = player1;
         this.player2 = player2;
+        this.data = data;
     }
-
-    public Button getBtnBackFromEndScreen() {
-        return btnBackFromEndScreen;
-    }
-
-    public GameBoard getBoard() {
-        return board;
-    }
-
 
     @FXML
     private Label winnerName;
 
     @FXML
     private Label winnerScore;
+
     @FXML
     private Button btnBackFromEndScreen;
 
@@ -55,28 +47,35 @@ public  class EndScreenController implements Initializable {
             final String INTRO_SCREEN_FILE_PATH = "/screen/view/IntroScreen.fxml";
 
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(INTRO_SCREEN_FILE_PATH));
-            fxmlLoader.setController(new IntroScreenController(board, player1, player2));
+            fxmlLoader.setController(new IntroScreenController(board, player1, player2, data));
             Parent root = fxmlLoader.load();
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
             stage.setScene(new Scene(root));
             stage.setTitle("Intro Screen");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        data.winnerNameProperty().addListener((observable, oldValue, newValue) -> {
-            winnerName.setText(newValue);
-        });
-        data.winnerScoreProperty().addListener(((observable, oldValue, newValue) -> {
-            winnerScore.setText(newValue);
-        }));
+        System.out.println("Initializing EndScreenController...");
+        System.out.println("winnerName: " + winnerName);  // Should print the label instance
+        System.out.println("winnerScore: " + winnerScore);  // Should print the label instance
+
+        // Ensure that the ShareData properties are properly bound
+        if (winnerName != null && winnerScore != null) {
+            // Binding the label texts to the ShareData properties
+            winnerName.textProperty().bind(data.winnerNameProperty());
+            winnerScore.textProperty().bind(data.winnerScoreProperty());
+
+            // Debug print to confirm property values
+            System.out.println("Winner Name: " + data.winnerNameProperty().get());
+            System.out.println("Winner Score: " + data.winnerScoreProperty().get());
+        } else {
+            System.out.println("FXML injection failed, labels are null.");
+        }
     }
+
+
 }
